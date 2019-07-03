5855baf04cdd3fb7c5f35915900e703c9f290770
Eva Rodriguez <eva22rodri@gmail.com>
2019-07-03 10:26:12 -0500

    Add project files.

---
+++ OpenHouseOrHouseVisit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OpenHouseOrHouseVisit
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            House house1 = new House();
+            house1.Address = "123 Main Street";
+
+            House house2 = new House();
+            house1.Address = "999 5th St";
+
+            house1.RingDoorbell();
+            house2.RingDoorbell();
+
+
+        }
+    }
+
+    class House
+    {
+        public string Address { get; set; }
+        public void RingDoorbell()
+        {
+            Console.WriteLine("Ding Dong! Is anyone home at " + Address + "?");
+        }
+    }
+}
